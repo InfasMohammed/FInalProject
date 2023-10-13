625533db64fe342aaeb2aaa1d1058530705c28de
Mohammed Infas <infasmohamed2018@gmail.com>
2023-10-13 22:57:56 +0530

    client added

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import Index from "./screen";
+import "./App.css";
+import InnerPage from "./screen/innerPage";
+import Portfolio from "./screen/portfolio";
+
+function App() {
+  return (
+    <div>
+      <Index />
+      <InnerPage />
+      <Portfolio />
+    </div>
+  );
+}
+
+export default App;
