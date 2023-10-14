790dcd38b684c69a4afddabfffe326e19f3598d3
ZiyahulAnsath <91413647+ZiyahulAnsath@users.noreply.github.com>
2023-10-14 19:40:21 +0530

    signin signup modification are completed

--- client/src/screen/Signup.jsx
+++ client/src/screen/Signup.jsx
@@ -1,73 +1,93 @@
 import { useState } from "react";
-import axios from 'axios'
+import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
-function Signup(){
-    const [name, setName] = useState()
-    const [email, setEmail] = useState()
-    const [password, setPassword] = useState()
-    const navigate = useNavigate()
+function Signup() {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const navigate = useNavigate();
 
-    // const handleSubmit = (e) => {
-    //     e.preventDefault()
-    //     axios.post('http://localhost:3001/register', {name,email,password})
-    //     .then(res =>{
-    //         alert("Created")
-    //         navigate('/login')
-    //     }).catch(err => console.log(err))
-    // }
-    
+  // const handleSubmit = (e) => {
+  //     e.preventDefault()
+  //     axios.post('http://localhost:3001/register', {name,email,password})
+  //     .then(res =>{
+  //         alert("Created")
+  //         navigate('/login')
+  //     }).catch(err => console.log(err))
+  // }
 
-
-return(
-    <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
-        <div className='bg-white p-3 rounded w-25'>
-            <h2>Signup</h2>
-            {/* <form onSubmit={handleSubmit}> */}
-            <form>
-                <div className='mb-3'>
-                    <label htmlFor='email'><strong>Name</strong></label>
-                    <input
-                    type='text' 
-                    placeholder='Enter Name' 
-                    autoComplete="off" 
-                    name='email'
-                    // onChange={(e) => setName(e.target.value)}
-                    className='form-control rounded-0'
-                    />
-                </div>
-                <div className='mb-3'>
-                    <label htmlFor='email'><strong>Email</strong></label>
-                    <input
-                    type='email' 
-                    placeholder='Enter Email' 
-                    autoComplete="off" 
-                    name='email'
-                    // onChange={(e) => setEmail(e.target.value)}
-                    className='form-control rounded-0'
-                    />
-                </div>
-                <div className='mb-3'>
-                    <label htmlFor='password'><strong>Password</strong></label>
-                    <input
-                    type='password' 
-                    placeholder='Enter Password' 
-                    autoComplete="off" 
-                    name='password'
-                    // onChange={(e) => setPassword(e.target.value)}
-                    className='form-control rounded-0'
-                    />
-                </div>
-                <button type='submit' className='btn btn-success w-100 rounded-0'>
-                    Register
-                </button>
-            </form>
-            <p>Already have an Account</p>
-            <Link to='/login' className='btn btn-success w-100 rounded-0'>
-                Login
+  return (
+    <>
+    <Header/>
+      <section id="footer">
+      <div className="d-flex justify-content-center align-items-center vh-100">
+        <div className="bg-warning-emphasis bg-gradient p-3 rounded w-25">
+          <h2 className="text-center">Signup</h2>
+          {/* <form onSubmit={handleSubmit}> */}
+          <form>
+            <div className="mb-3">
+              <label htmlFor="email">
+                <strong>Name</strong>
+              </label>
+              <input
+                type="text"
+                placeholder="Enter Name"
+                autoComplete="off"
+                name="email"
+                // onChange={(e) => setName(e.target.value)}
+                className="form-control rounded-0.5 text-body-secondary"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="email">
+                <strong>Email</strong>
+              </label>
+              <input
+                type="email"
+                placeholder="Enter Email"
+                autoComplete="off"
+                name="email"
+                // onChange={(e) => setEmail(e.target.value)}
+                className="form-control rounded-0.5 text-body-secondary"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password">
+                <strong>Password</strong>
+              </label>
+              <input
+                type="password"
+                placeholder="Enter Password"
+                autoComplete="off"
+                name="password"
+                // onChange={(e) => setPassword(e.target.value)}
+                className="form-control rounded-0.5 text-body-secondary"
+              />
+            </div>
+            <div className="text-center">
+              <button
+                type="submit"
+                className="signup shadow-lg w-50 rounded-5 text-center"
+              >
+                Signup
+              </button>
+            </div>
+          </form>
+          <br />
+          <p className="text-align-center">
+            Already have an Account{" "}
+            <Link to={"/login"} className="text-warning rounded">
+              Login
             </Link>
+          </p>
         </div>
-    </div>
-)
+      </div>
+      </section>
+      <Footer/>
+    </>
+  );
 }
 export default Signup;
