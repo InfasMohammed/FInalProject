e13c7275723376244664be63a1ce44a13c3f9955
Mohammed Infas <infasmohamed2018@gmail.com>
2023-10-15 23:37:23 +0530

    changes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,8 +8,6 @@ const Header = () => {
         <h1 className="logo">
           <a href="/">Smart Invest</a>
         </h1>
->>>>>>> 790dcd38b684c69a4afddabfffe326e19f3598d3
-
         <nav id="navbar" className="navbar">
           <ul>
             <li>
