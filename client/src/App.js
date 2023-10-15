f96fce9481a511bebad0478ae33ed6777a32f996
ZiyahulAnsath <91413647+ZiyahulAnsath@users.noreply.github.com>
2023-10-15 23:13:11 +0530

    about, and contact us components are created

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,21 @@ import Home from "./screen/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Signup from "./screen/Signup";
 import Login from "./screen/Login";
+import About from "./screen/About";
+import Contact from "./screen/Contact";
 
 function App() {
   return (
     <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/signup' element={<Signup/>}/>
-          <Route path='/login' element={<Login/>}/>
-          {/* <Route path='/dashboard' element={<Dashboard/>}/> */}
-          
-          
-          
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        {/* <Route path='/dashboard' element={<Dashboard/>}/> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
