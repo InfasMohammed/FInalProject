df94d467973d3ceff2570afeb935c82bae883551
Mohammed Infas <infasmohamed2018@gmail.com>
2023-10-25 17:52:43 +0530

    Added server

---
+++ server/routes/admin.js
@@ -0,0 +1,136 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const Admin = require('../models/Admin');
+
+// Admin login
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const admin = await Admin.findOne({ email });
+
+    if (!admin) {
+      return res.json({ success: false, message: "Admin not found" });
+    }
+
+    const validPassword = await bcrypt.compare(password, admin.password);
+
+    if (!validPassword) {
+      return res.json({ success: false, message: "Invalid password" });
+    }
+
+    // Generate JWT token
+    const token = jwt.sign({ id: admin._id }, process.env.JWT_SECERT_KEY);
+
+    const { _id, name } = admin;
+
+    return res.status(200).json({
+      message: "Successfully logged in",
+      admin: {
+        email: admin.email,
+        _id,
+        name,
+      },
+      token,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      error: "An error occurred while logging",
+    });
+  }
+});
+
+
+// Get all admins
+router.get('/admins', async (req, res) => {
+  try {
+    const admins = await Admin.find();
+    res.json(admins);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+// Get admin by ID
+router.get('/admins/:id', async (req, res) => {
+  try {
+    const admin = await Admin.findById(req.params.id);
+    if (!admin) {
+      return res.status(404).json({ message: 'Admin not found' });
+    }
+    res.json(admin);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+// Create a new admin
+router.post('/admins', async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // Check if the admin already exists
+    const existingAdmin = await Admin.findOne({ email });
+    if (existingAdmin) {
+      return res.status(409).json({ message: 'Admin already exists' });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new admin
+    const admin = new Admin({ name, email, password: hashedPassword });
+    await admin.save();
+
+    res.status(201).json({ message: 'Admin created successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// Update an admin
+router.put('/admins/:id', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const admin = await Admin.findByIdAndUpdate(
+      req.params.id,
+      { email, password },
+      { new: true }
+    );
+
+    if (!admin) {
+      return res.status(404).json({ message: 'Admin not found' });
+    }
+
+    res.json(admin);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+// Delete an admin
+router.delete('/admins/:id', async (req, res) => {
+  try {
+    const admin = await Admin.findByIdAndRemove(req.params.id);
+
+    if (!admin) {
+      return res.status(404).json({ message: 'Admin not found' });
+    }
+
+    res.json({ message: 'Admin deleted successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+module.exports = router;
